7ef2bfbc7e71d33d8c2d22f52a81f967744167a4
matthew-charles-chan <matthew.charles.chan@gmail.com>
2020-02-04 20:42:57 +0000

    create teachers/ as_requests tables and seed tables

--- 3_quiries_group-by/6_low_avg_completion.sql
+++ 3_quiries_group-by/6_low_avg_completion.sql
@@ -1,11 +1,11 @@
--- SELECT students.name as students, avg(assignment_submissions.duration) as average_assignment_duration, avg(assignments.duration) as average_estimated_duration
---   FROM students
---   JOIN assignment_submissions ON students.id = student_id
---   JOIN assignments ON assignment_id = assignments.id
---   WHERE end_date IS NULL
---   GROUP BY students.name
---   HAVING avg(assignment_submissions.duration) < avg(assignments.duration)
---   ORDER BY avg(assignment_submissions.duration);
+SELECT students.name as students, avg(assignment_submissions.duration) as average_assignment_duration, avg(assignments.duration) as average_estimated_duration
+  FROM students
+  JOIN assignment_submissions ON students.id = student_id
+  JOIN assignments ON assignment_id = assignments.id
+  WHERE end_date IS NULL
+  GROUP BY students.name
+  HAVING avg(assignment_submissions.duration) < avg(assignments.duration)
+  ORDER BY avg(assignment_submissions.duration);
 
 -- SELECT (Select count(*) FROM assignments) - count(assignment_submissions) as total_incomplete
 --   FROM assignment_submissions
@@ -29,12 +29,12 @@
 --   JOIN students ON students.id = student_id
 --   WHERE students.name = 'Ibrahim Schimmel';
 
-SELECT assignments.name 
-  FROM assignments
-  WHERE assignments.id NOT IN 
-  ( 
-    SELECT assignment_id
-    FROM assignment_submissions
-    JOIN students ON students.id = student_id
-    WHERE students.name = 'Ibrahim Schimmel'
-  );
+-- SELECT assignments.name 
+--   FROM assignments
+--   WHERE assignments.id NOT IN 
+--   ( 
+--     SELECT assignment_id
+--     FROM assignment_submissions
+--     JOIN students ON students.id = student_id
+--     WHERE students.name = 'Ibrahim Schimmel'
+--   );
