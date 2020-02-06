6f66896509e7c1b7c5be7324b3082b35b40c9ceb
matthew-charles-chan <matthew.charles.chan@gmail.com>
2020-02-06 02:15:58 +0000

    implement parameterized queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -9,14 +9,18 @@ const pool = new Pool({
 
 
 const arguments = process.argv.slice(2);
+const cohortName = arguments[0];
+const limit = arguments[1] || 5;
+
+const values = [`%${cohortName}%`, limit]
 
 pool.query(`
 SELECT students.id as student_id, students.name as student_name, cohorts.name as cohort_name
 FROM students
 JOIN cohorts on cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${arguments[0]}%'
-LIMIT ${arguments[1] || 5};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.student_name} has an id of ${user.student_id} and was in the ${user.cohort_name} cohort`)
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -10,20 +10,7 @@ const pool = new Pool({
 
 const arguments = process.argv.slice(2);
 
-// pool.query(`
-// SELECT students.id as student_id, students.name as student_name, cohorts.name as cohort_name
-// FROM students
-// JOIN cohorts on cohort_id = cohorts.id
-// WHERE cohorts.name LIKE '%${arguments[0]}%'
-// LIMIT ${arguments[1] || 5};
-// `)
-// .then(res => {
-//   res.rows.forEach(user => {
-//     console.log(`${user.student_name} has an id of ${user.student_id} and was in the ${user.cohort_name} cohort`)
-//   })
-// })
-// .catch(err => {console.log('query error', err.stack)});
-
+const values = [`${arguments[0]}`];
 
 pool.query(`
   
@@ -32,10 +19,10 @@ FROM teachers
 JOIN assistance_requests on teachers.id = teacher_id
 JOIN students on student_id = students.id
 JOIN cohorts on cohort_id = cohorts.id
-WHERE cohorts.name = '${arguments[0]}'
+WHERE cohorts.name = $1
 ORDER BY teachers.name;
 
-`)
+`, values)
 .then( res => {
   res.rows.forEach(teacher => {
     console.log(`${teacher.cohort}: ${teacher.teacher}`)
